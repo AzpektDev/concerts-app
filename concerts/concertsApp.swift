0e0a327baba8bae0ec42f8cbe29c2b7ac133001a
Azpekt <65199167+AzpektDev@users.noreply.github.com>
2025-06-13 18:42:58 +0200

    initial commit

---
+++ concerts/concertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  concertsApp.swift
+//  concerts
+//
+//  Created by Eryk Janiczek on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct concertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ concertsTests/concertsTests.swift
@@ -0,0 +1,17 @@
+//
+//  concertsTests.swift
+//  concertsTests
+//
+//  Created by Eryk Janiczek on 6/13/25.
+//
+
+import Testing
+@testable import concerts
+
+struct concertsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
